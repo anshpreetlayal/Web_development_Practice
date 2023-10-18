38ee8fec9a1491a84202869acdc73f44a0885cf9
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-10-17 20:19:00 -0400

    editing

--- To-do_List/to-do.js
+++ To-do_List/to-do.js
@@ -5,3 +5,11 @@ import { View, Text, TextInput, Button, FlatList } from 'react-native';
 export default function App() {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
+
+  
+  const addTask = () => {
+    if (task.trim() !== '') {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
